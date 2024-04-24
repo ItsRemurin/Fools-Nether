59c0274a56b5a9faf58dc9a31ce8897b79b45c6a
ItsRemurin <ItsRemurin@gmail.com>
2024-04-24 16:32:31 -0400

    Initial commit

---
+++ src/main/java/com/itsremurin/FoolsNetherMod.java
@@ -0,0 +1,17 @@
+package com.itsremurin;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FoolsNetherMod implements ModInitializer {
+
+	public static final String MOD_ID = "fools_nether";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
